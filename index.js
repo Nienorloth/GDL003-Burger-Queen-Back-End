4c09d7f491aca6f3961da78e567ed87de78da5de
peraltadnni <dnisse-x00@hotmail.com>
2019-09-27 18:29:13 -0500

    Adding simple server and four end points

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const routes = require("./src/routes/crmRoutes");
 
 const app = express();
 const PORT = 3000;
 
+routes(app);
+
+//Starting the server
 app.get("/", (req, res) =>  res.send( `Node and express server is running on port ${PORT}` )
 );
 
- app.listen(PORT, () => 
+app.listen(PORT, () => 
     console.log(`Your server is runing on port ${PORT}`)
- );
+);
