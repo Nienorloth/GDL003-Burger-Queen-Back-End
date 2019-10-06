051cd4530601c8c9b7e3435eb640945dab2c4a00
peraltadnni <dnisse-x00@hotmail.com>
2019-10-06 18:59:40 -0500

    Implementing env into the index.js

--- index.js
+++ index.js
@@ -33,27 +33,7 @@ routes(app);
 app.get('/', (req, res) => {
     res.json({ "message": "Welcome to Kawaii Burgers"});
 });
-// Register the route to get a new token
-// In a real world scenario we would authenticate user credentials
-// before creating a token, but for simplicity accessing this route
-// will generate a new token that is valid for 2 minutes
-app.get('/token', function(req, res){
-    var token = jwt.sign({username:"ado"}, 'supersecret',{expiresIn: 120});
-    res.send(token)
-  })
-  
-  // Register a route that requires a valid token to view data
-  app.get('/api', function(req, res){
-    var token = req.query.token;
-    jwt.verify(token, 'supersecret', function(err, decoded){
-      if(!err){
-        var secrets = {"accountNumber" : "938291239","pin" : "11289","account" : "Finance"};
-        res.json(secrets);
-      } else {
-        res.send(err);
-      }
-    })
-  })
+
 // Launch our app on port
 app.listen(process.env.PORT, () => 
     console.log(`Your server is runing on port ${process.env.PORT}`)
