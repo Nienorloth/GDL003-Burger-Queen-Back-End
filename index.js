453e5e1b4a408037e61ffd16f36364a8383c562f
Nienorloth <nienorloth@gmail.com>
2019-10-10 18:33:30 -0500

    cors again

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const cors require('cors');
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
